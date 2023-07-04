b49a92b752dfd7a1b8cb07f04b67b4805254c017
Pranav Verma <pverma@mathworks.com>
2023-07-04 18:39:39 +0530

    Removing dotenv dependency

--- tests/configuration/find_port.py
+++ tests/configuration/find_port.py
@@ -1,10 +1,7 @@
 import os
 import socket
-from dotenv import load_dotenv, set_key
 
 # Load the environment variables from the .env file
-load_dotenv()
-
 def find_free_port():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as temp_socket:
         temp_socket.bind(('localhost', 0))
