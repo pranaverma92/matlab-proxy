20d30426df5d6ff111f1c0ae56e13914e656d980
Pranav Verma <pverma@mathworks.com>
2024-06-19 15:45:16 +0530

    adding matlab tests

--- tests/matlab-tests/testStartupScript.m
+++ tests/matlab-tests/testStartupScript.m
@@ -4,39 +4,46 @@ classdef testStartupScript < matlab.unittest.TestCase
         LogDir
         LogFile
     end
-    
+
+    methods (TestClassSetup)
+        function addFunctionPath(testCase)
+            addpath('../../matlab_proxy/matlab/')
+            addpath('../../tests/matlab-tests/')
+        end
+    end
+
+    methods (TestClassTeardown)
+        function removeFunctionPath(testCase)
+            rmpath('../../matlab_proxy/matlab/')
+            rmpath('../../tests/matlab-tests/')
+        end
+    end
+
     methods (TestMethodSetup)
         function setupEnvironmentVariables(testCase)
             testCase.OriginalMWI_MATLAB_STARTUP_SCRIPT = getenv('MWI_MATLAB_STARTUP_SCRIPT');
             testCase.LogDir = tempname;
-            disp("testCase.LogDir")
-            disp(testCase.LogDir)
-            disp("testCase.LogDir end")
             mkdir(testCase.LogDir);
             setenv('MATLAB_LOG_DIR', testCase.LogDir);
             testCase.LogFile = fullfile(testCase.LogDir, "startup_code_output.txt");
         end
     end
-    
+
     methods (TestMethodTeardown)
         function restoreEnvironmentVariables(testCase)
             setenv('MWI_MATLAB_STARTUP_SCRIPT', testCase.OriginalMWI_MATLAB_STARTUP_SCRIPT);
             rmdir(testCase.LogDir, 's');
         end
     end
-    
+
     methods (Test)
         function testSuccessfulStartupCodeExecution(testCase)
             % Using a command without spaces
             setenv('MWI_MATLAB_STARTUP_SCRIPT', 'disp(42)');
             % runYourScriptHere; % Replace with the command to run your script
             run('evaluateUserMatlabCode.m')
-
             % Verify the log file contains the expected output
             text = fileread(testCase.LogFile);
-            disp("text")
-            disp(text)
-            disp("text end")
             testCase.verifyTrue(contains(text, '42'));
         end
     end
